2a5499a93352cce50830a257b1df62b71b21475a
Nakib Iqbal <nakibiqbal.2006@gmail.com>
2025-02-28 12:19:44 +0600

    Eightth Section added

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Section_6 from './PAGES/SECTION_6/Section_6'
 import Section_7 from './PAGES/SECTION_7/Section_7'
 import Section_10 from './PAGES/SECTION_10/Section_10'
 import Section_9 from './PAGES/SECTION_9/Section_9'
+import Section_8 from './PAGES/SECTION_8/Section_8'
 
 function App() {
   useLenis()
@@ -23,6 +24,7 @@ function App() {
       <Right_Img />
       <Section_6 />
       <Section_7 />
+      <Section_8 />
       <Section_9 />
       <Section_10 />
       <div style={{ width: "100%", height: "100vh", background: "white" }}></div>
---
+++ src/PAGES/SECTION_8/Section_8.jsx
@@ -0,0 +1,46 @@
+import useAnimationBasic from "../../ANIMATIONS/useAnimationBasic";
+import { useClipPathAnimation } from "../../ANIMATIONS/useClipPathAnimation";
+import "./Section_8.css";
+import { motion } from "framer-motion";
+
+const Section_8 = () => {
+    const { textRef1 } = useAnimationBasic();
+    const text = "“Sundae Creative is a trailblazer in influencer marketing, connecting brands with influential voices across social media to build impactful messages.”"
+    return (
+        <section id="section_8">
+            <div className="contentWrapper8">
+
+                <div className="imgRight8">
+                    <motion.img {...useClipPathAnimation} src="https://sundaecreative.com/uploads/pages/home/14_450_Small_image-450w.jpg" />
+                </div>
+
+                <div className="mainContent8">
+
+                    <div className="leftImg8">
+                        <img src="https://sundaecreative.com/uploads/pages/home/13_450_Small_image-450w.jpg" />
+                    </div>
+                    <div className="textFounder">
+                        <div ref={textRef1} className="parentForTxt">
+                            {text.split(" ").map((word, index) => (
+                                <div className="word for8" key={index}>
+                                    <h3 className="xyrAnim">{word}</h3>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="founder">
+                            <motion.img {...useClipPathAnimation} src="https://sundaecreative.com/uploads/attachments/quote/16_Quote_Kristina_450_Small_image-450w.jpg" />
+                            <div className="founderInfo">
+                                <span>Kristina Argento</span>
+                                <span>Co-founder</span>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default Section_8
